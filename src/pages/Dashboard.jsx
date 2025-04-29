2776423137cd514cf74cdc3466c65c1508dae666
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-04-29 20:08:55 +0500

    done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import { Context } from "../context/userContext";
-
-// const Dashboard = () => {
-//   const { taskList } = Context();
-//   return (
-//     <div>
-//       <div className="task">
-//         <h1>Total Task</h1>
-//         <p>{taskList.length}</p>
-//       </div>
-//       <div className="task">
-//         <h1>Completed Task</h1>
-//         <p>{taskList.length}</p>
-//       </div>
-//       <div className="task">
-//         <h1>Pending Task</h1>
-//         <p>{taskList.length}</p>
-//       </div>
-//       <div className="task">
-//         <h1>Todo Task</h1>
-//         <p>{taskList.length}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
 import React from "react";
 import { Context } from "../context/TaskContext";
 
